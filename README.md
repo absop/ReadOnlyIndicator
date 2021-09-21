0a95ffe2ec223509d5bb841b2e54d9a676b3d773
叶芝秋 <zlang0@163.com>
2021-09-21 22:39:42 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ReadOnlyIndicator
+
+A plugin for Sublime Text to toggle the **read-only** mode of view, and indicate whether the view is read only or not.
+
+## Command
+```json
+[
+  {
+    "caption": "ReadOnlyIndicator: Toggle Read Only",
+    "command": "toggle_read_only"
+  }
+]
+```
+
+## Settings
+```json
+{
+    "readonly_indicator": "🔒",
+    "editable_indicator": "🔓"
+}
+```
+
+You can change the `indicators` from emojis to plain strings.
---
+++ main.py
@@ -0,0 +1,65 @@
+import sublime
+import sublime_plugin
+
+
+class ToggleReadOnlyCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.view.set_read_only(not self.view.is_read_only())
+    indicator.show_read_only_status(self.view)
+
+
+class ReadOnlyIndicator(sublime_plugin.EventListener):
+    def on_new_async(self, view):
+        indicator.show_read_only_status(view)
+
+    def on_load_async(self, view):
+        indicator.show_read_only_status(view)
+
+
+class Indicator():
+    status_key = '__read_only'
+
+    def __init__(self, settings_name='ReadonlyIndicator.sublime-settings'):
+        self.settings_name = settings_name
+
+    def __getitem__(self, readonly: bool):
+        return self.readonly_emoji if readonly else self.editable_emoji
+
+    def stop(self):
+        self.iter_views(self.clear_read_only_status)
+
+    def start(self):
+        settings = sublime.load_settings(self.settings_name)
+        settings.add_on_change('readonly_indicator',
+            lambda: self.update_read_only_status(settings))
+        self.update_read_only_status(settings)
+
+    def load_emojis(self, settings):
+        self.readonly_emoji = settings.get('readonly_indicator') or '🔒'
+        self.editable_emoji = settings.get('editable_indicator') or '🔓'
+
+    def update_read_only_status(self, settings):
+        self.load_emojis(settings)
+        self.iter_views(self.show_read_only_status)
+
+    def show_read_only_status(self, view):
+        view.set_status(self.status_key, self[view.is_read_only()])
+
+    def clear_read_only_status(self, view):
+        view.erase_status(self.status_key)
+
+    def iter_views(self, f):
+        for window in sublime.windows():
+            for view in window.views():
+                f(view)
+
+
+indicator = Indicator()
+
+
+def plugin_loaded():
+    indicator.start()
+
+
+def plugin_unloaded():
+    indicator.stop()
